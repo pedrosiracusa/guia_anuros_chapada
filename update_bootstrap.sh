007c0073a2b5c8b9e6b326c023a7f764102151fd
Pedro Correia <pedro.siracusa@serpro.gov.br>
2025-03-20 16:58:47 -0300

    feat(sound-player): Incorporando o código da howler.js ao projeto

--- update_bootstrap.sh
+++ update_bootstrap.sh
@@ -4,16 +4,12 @@
 
 rm -rf node_modules package-lock.json
 mkdir node_modules
-npm install bootstrap@5 jquery@3 bootstrap-icons
+npm install bootstrap@5 jquery@3 bootstrap-icons howler
 
 rm -rf _sass/bootstrap
 mkdir -p _sass/bootstrap
 cp -r node_modules/bootstrap/scss/* _sass/bootstrap
 touch _sass/bootstrap/__DO_NOT_MODIFY
-
-rm -rf assets/bootstrap-icons
-cp -r node_modules/bootstrap-icons assets/bootstrap-icons
-touch assets/bootstrap-icons/__DO_NOT_MODIFY
 
 rm -rf assets/javascript/bootstrap
 mkdir -p assets/javascript/bootstrap
@@ -21,3 +17,10 @@ cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.* assets/javascript/boots
 cp node_modules/jquery/dist/jquery.min.* assets/javascript/bootstrap/
 touch assets/javascript/bootstrap/__DO_NOT_MODIFY
 
+rm -rf assets/bootstrap-icons
+cp -r node_modules/bootstrap-icons assets/bootstrap-icons
+touch assets/bootstrap-icons/__DO_NOT_MODIFY
+
+rm -rf assets/javascript/howler
+cp -r node_modules/howler/dist assets/javascript/howler
+touch assets/javascript/howler/__DO_NOT_MODIFY
