f9b3a1a874c0518d1b6a9018e4573768d91eb2fe
Pedro Correia <pedro.siracusa@serpro.gov.br>
2025-03-20 16:33:46 -0300

    feat(sound-player): Ajustando controles do player, adicionando vocalizações

--- assets/javascript/species-sounds.js
+++ assets/javascript/species-sounds.js
@@ -1,25 +1,48 @@
-var sound = new Howl({
-    src: ['frogsound.wav']
-})
-
-console.log(sound)
-//sound.play();
-
-sound.on("stop",()=>{
-    document.querySelector("#sound-player #ctrl-play").style.display = "inline-block"
-    document.querySelector("#sound-player #ctrl-pause").style.display = "none"
-})
-sound.on("play",()=>{
-    document.querySelector("#sound-player #ctrl-play").style.display = "none"
-    document.querySelector("#sound-player #ctrl-pause").style.display = "inline-block"
-})
-
 var soundIsPlaying=false;
+var loopCounter=0;
+
 
 if(document.querySelector(".species-page")){
 
+    const speciesId = document.URL.substr( document.URL.lastIndexOf('/')+1)
+    const fileUrl = `../assets/audio/species_calls/${speciesId}`
+
+    var sound = new Howl({
+        src: [`${fileUrl}.wav`,`${fileUrl}.mp3`,`${fileUrl}.webm`],
+        autoplay: false,
+        loop:true,
+        onstop: onStop,
+        onplay: onPlay,
+        onend: onEnd
+    })
+
+
+    function onStop(){
+        document.querySelector("#sound-player #ctrl-play").style.display = "inline-block"
+        document.querySelector("#sound-player #ctrl-pause").style.display = "none"
+        loopCounter=0
+    }
+    
+    function onPlay(id){
+        document.querySelector("#sound-player #ctrl-play").style.display = "none"
+        document.querySelector("#sound-player #ctrl-pause").style.display = "inline-block"
+    }
+
+    function onEnd(){
+        loopCounter+=1
+        if (loopCounter>3)
+            this.stop()
+    }
+
+    sound.once("load",()=>{
+        showMediaControls()
+    })
+
+    function showMediaControls(){
+        document.querySelector("#sound-player").style.display="flex";
+    }
+
     document.querySelector("#sound-player").addEventListener('click',(e)=>{
-        console.log(sound.playing())
         if( sound.playing() )
             sound.stop()
         else
