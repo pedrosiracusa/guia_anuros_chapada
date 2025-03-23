1d57d909155b47613d8db028aab11fa7066c1e19
Pedro Correia <pedro.siracusa@serpro.gov.br>
2025-03-23 19:05:47 -0300

    feat(diagnose): Adicionando imagens de diagnose nas páginas das espécies

---
+++ assets/javascript/species-diagnose.js
@@ -0,0 +1,14 @@
+---
+---
+
+if(document.querySelector("div.species-page")){ // Only runs in species pages
+    const speciesId = document.URL.substr( document.URL.lastIndexOf('/')+1)
+    diagnoseDiv = document.createElement("div")
+    diagnoseDiv.innerHTML = `<img src="{{site.baseurl}}/assets/img/diagnose/${speciesId}.jpg">`
+
+    document.querySelector(".diagnose").appendChild( diagnoseDiv)
+
+    document.querySelector(".diagnose img").addEventListener("error",(e)=>{
+        e.originalTarget.style.display="none";
+    })
+}
