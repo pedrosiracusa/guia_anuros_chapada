1755377c95408b516bf2a800dfe67a7b0c82424d
Pedro Correia <pedro.siracusa@serpro.gov.br>
2025-03-23 20:34:31 -0300

    style(diagnose): Alguns ajustes de tela

--- assets/javascript/species-diagnose.js
+++ assets/javascript/species-diagnose.js
@@ -4,6 +4,8 @@
 if(document.querySelector("div.species-page")){ // Only runs in species pages
     const speciesId = document.URL.substr( document.URL.lastIndexOf('/')+1)
     diagnoseDiv = document.createElement("div")
+    diagnoseDiv.classList.add("col-xl-6", "col-lg-8")
+    diagnoseDiv.id = "img-diagnose"
     diagnoseDiv.innerHTML = `<img src="{{site.baseurl}}/assets/img/diagnose/${speciesId}.jpg">`
 
     document.querySelector(".diagnose").appendChild( diagnoseDiv)
