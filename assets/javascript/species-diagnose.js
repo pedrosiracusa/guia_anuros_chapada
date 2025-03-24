4554478439602cc0436f5ee7be9b0d538f9cda76
Pedro Correia <pedro.siracusa@serpro.gov.br>
2025-03-23 21:10:23 -0300

    fix(diagnose): Ajustando para quando não houver imagem da diagnose

--- assets/javascript/species-diagnose.js
+++ assets/javascript/species-diagnose.js
@@ -2,15 +2,22 @@
 ---
 
 if(document.querySelector("div.species-page")){ // Only runs in species pages
-    const speciesId = document.URL.substr( document.URL.lastIndexOf('/')+1)
-    diagnoseDiv = document.createElement("div")
-    diagnoseDiv.classList.add("col-xl-6", "col-lg-8")
-    diagnoseDiv.id = "img-diagnose"
-    diagnoseDiv.innerHTML = `<img src="{{site.baseurl}}/assets/img/diagnose/${speciesId}.jpg">`
 
-    document.querySelector(".diagnose").appendChild( diagnoseDiv)
 
-    document.querySelector(".diagnose img").addEventListener("error",(e)=>{
-        e.originalTarget.style.display="none";
+    window.addEventListener('load', function(){
+        const speciesId = document.URL.substr( document.URL.lastIndexOf('/')+1)
+        diagnoseDiv = document.createElement("div")
+        diagnoseDiv.classList.add("col-xl-6", "col-lg-8")
+        diagnoseDiv.id = "img-diagnose"
+        diagnoseDiv.innerHTML = `<img src="{{site.baseurl}}/assets/img/diagnose/${speciesId}.jpg">`
+    
+        document.querySelector(".diagnose").appendChild( diagnoseDiv)
+    
+        document.querySelector(".diagnose img").addEventListener("error",(e)=>{
+            e.currentTarget.style.display="none";
+        })
     })
+
+
+    
 }
